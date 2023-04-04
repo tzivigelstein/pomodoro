1fec325672059439ad0075a825fc5158ffcfe47e
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-04-04 17:26:38 -0300

    update progress component

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -5,23 +5,32 @@ import styles from './index.module.css'
 interface Props {
   time: number
   currentStatus: Status
+  showStage: boolean
 }
 
-export default function Progress({ time, currentStatus }: Props) {
+export default function Progress({ time, showStage, currentStatus }: Props) {
   const divisor = useMemo(() => {
     return 360 / (time / 60)
   }, [currentStatus])
 
+  const currentStatusCapitalized =
+    currentStatus[0].toUpperCase() + currentStatus.slice(1)
+
   return (
-    <div
-      className={styles.progress}
-      style={{
-        background: `conic-gradient(var(${
-          currentStatus === Status.resting ? '--resting-accent' : '--accent'
-        }) ${(time / 60) * divisor}deg, white 0deg)`,
-      }}
-    >
-      <span className={styles.time}>{Math.ceil(time / 60)}</span>
+    <div className={styles.container}>
+      <div
+        className={styles.progress}
+        style={{
+          background: `conic-gradient(var(${
+            currentStatus === Status.resting ? '--resting-accent' : '--accent'
+          }) ${(time / 60) * divisor}deg, white 0deg)`,
+        }}
+      >
+        <span className={styles.time}>{Math.ceil(time / 60)}</span>
+      </div>
+      {showStage && (
+        <span className={styles.currentStatus}>{currentStatusCapitalized}</span>
+      )}
     </div>
   )
 }
